c814bb0b2041caa2226cd01c580f3b5fc181dcf9
radjah31 <radjahamin@gmail.com>
2017-04-24 21:14:25 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("a is : " + a + " Hello Word !");
+            Console.ReadKey();
+        }
+    }
+}
